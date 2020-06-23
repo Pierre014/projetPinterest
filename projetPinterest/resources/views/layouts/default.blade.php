1f2b7a18939fc5e98c346aab2285b58def133d91
Amaury Deltenre <amaury.deltenre@gmail.com>
2020-06-23 11:12:42 +0200

    create profil.blade.php and default.blade.php

---
+++ projetPinterest/resources/views/layouts/default.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href={{URL::asset('/css/bootstrap.min.css')}}>
+    <title>PInterest @yield('title')</title>
+</head>
+
+<body>
+    @yield('section')
+</body>
+
+</html>
---
+++ projetPinterest/resources/views/profil.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.default')
+
+@section('title', 'profil')
+
+@section('content')
+
+<!-- Section -->
+<h1>profil</h1>
+
+@endsection
