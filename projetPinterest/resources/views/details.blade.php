be60d8f024ede74cffd4de101fc08903e2010930
BL <benoit.lena@protonmail.com>
2020-06-25 14:03:36 +0200

    detail page/detail controller/route

--- projetPinterest/resources/views/details.blade.php
+++ projetPinterest/resources/views/details.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 <div class="container">
-    <h1 class="m-3 ">▪ Nom Picture DB ▪</h1>
+    <h1 class="m-3">▪ Nom Picture DB ▪</h1>
     <div class="row">
       <div class="col-md-8">
         <a href="http://placehold.it/750x500"><img class="img-fluid rounded" src="http://placehold.it/750x500" alt=""></a>
@@ -11,13 +11,13 @@
       <div class="col-md-4 my-auto text-right">
         <h3 class="my-3 font-weight-bold">Description</h3>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ea numquam fugiat sit iusto. Obcaecati mollitia, accusamus eligendi totam illo voluptates facere id error eius deserunt, modi enim, voluptatibus tempora.</p>
-        <h3 class="my-3 font-weight-bold">Author</h3>
+        <h3 class="my-3">Author</h3>
         <ul class="list-unstyled">
-          <li><a href="{{asset(Auth::user()->pseudo)}}">need author replace pseudo J-J Goldman</a></li>
+          <li><a href="#">need author replace pseudo J-J Goldman</a></li>
         </ul>
-        <h3 class="my-3 font-weight-bold">Wanna Pint'?</h3>
+        <h3 class="my-3">Wanna Pint'?</h3>
         <ul class="list-unstyled">
-          <li><a href="{{asset(Auth::user()->pseudo)}}"><button type="submit" class="btn  text-white btn-info">Shares this</button></a></li>
+          <li><a href="#"><button type="submit" class="btn  text-white btn-info">Shares this</button></a></li>
           
         </ul>
       </div>
