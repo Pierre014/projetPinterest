11b08e548280b1a81dad80338112c5cac0031966
BL <benoit.lena@protonmail.com>
2020-06-26 12:05:15 +0200

    button + page share

--- projetPinterest/resources/views/share.blade.php
+++ projetPinterest/resources/views/share.blade.php
@@ -8,20 +8,22 @@
             <h1 class="text-center">Wanna share somethings?</h1>
         </div>
     </div>
-    <div class="row text-center m-3">
-        <div class="col-lg-6">
+    <div class="row m-3">
+        <div class="col-lg-6 input-group">
             <img class="img-fluid rounded" src="http://placehold.it/750x500" alt="waiting">
+                <input class="input-group input-group-lg custom-control-file mt-2" type="file" id="file" >
         </div>
+        
 
-        <div class="col-lg-6">
+        <div class="col-lg-6 ">
             <div class="input-group input-group-lg mb-5">
                 <div class="input-group-prepend">
                   <span class="input-group-text" id="inputGroup-sizing-lg">Title</span>
                 </div>
                 <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg">
             </div>
-            <textarea class="form-control" id="exampleFormControlTextarea4" rows="10"></textarea>
-            <input class="custom-control-file m-2 border border-dark btn btn-secondary btn-lg" type="file" id="file" class="custom-file-input" placeholder="Description" >
+            <textarea class="form-control" id="exampleFormControlTextarea4" rows="14" placeholder="Description"></textarea>
+            <button type="submit" class="btn btn-lg btn-danger border border-dark m-2 float-right">Share it !</button>
         </div>
         
     </div>
